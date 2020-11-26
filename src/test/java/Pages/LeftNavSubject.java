07dd82fe761c33c845267b9819fb894e063c48c8
ctntprk63 <cetintoprak0863@gmail.com>
2020-11-26 21:54:26 +0100

    SubjectSteps

---
+++ src/test/java/Pages/LeftNavSubject.java
@@ -0,0 +1,79 @@
+package Pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LeftNavSubject extends ParentClassSubject{
+
+    public LeftNavSubject() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//span[text()='Education']")
+    private WebElement education;
+
+    @FindBy(xpath = "(//span[text()='Setup'])[5]")
+    private  WebElement setupEducation;
+
+    @FindBy(xpath = "(//span[text()='Subjects'])[1]")
+    private  WebElement subject;
+
+    @FindBy(xpath = "(//span[text()='Subject Categories'])[1]")
+    public WebElement SubjectCategoriesButton;
+
+
+
+    @FindBy(linkText = "Human Resources")
+    private WebElement humanButton;
+
+    @FindBy(xpath = "//span[@class='nav-link-title ng-tns-c103-21 ng-star-inserted']")
+    private WebElement humanSetupButton;
+
+    @FindBy(linkText = "Salary Modifiers")
+    private WebElement SalaryModfButton;
+
+
+
+    WebElement myElement;
+    public void findElementAndClickFunction(String elementName){
+
+        switch (elementName ){
+
+            case "education" :
+                myElement =education;
+                break;
+
+            case "setupEducation" :
+                myElement =setupEducation;
+                break;
+
+            case "subject" :
+                myElement =subject;
+                break;
+
+            case "SubjectCategoriesButton" :
+                myElement =SubjectCategoriesButton;
+                break;
+
+
+            case "humanButton" :
+                myElement =humanButton;
+                break;
+
+
+            case "humanSetupButton" :
+                myElement =humanSetupButton;
+                break;
+
+            case "SalaryModfButton" :
+                myElement =SalaryModfButton;
+                break;
+
+
+        }
+
+        clickFunction(myElement);
+
+    }
+}
