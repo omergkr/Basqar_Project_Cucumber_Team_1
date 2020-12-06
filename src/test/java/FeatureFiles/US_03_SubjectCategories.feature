9354ad765bc8de0c939cab0402071e870770f96a
HulyaKonyali <konyali.hly@gmail.com>
2020-12-06 23:09:41 +0100

    Güncel Son

--- src/test/java/FeatureFiles/US_03_SubjectCategories.feature
+++ src/test/java/FeatureFiles/US_03_SubjectCategories.feature
@@ -8,50 +8,46 @@ Feature: Education_Setup_SubjectCategories
 
   Scenario: US_03 TC_01
     When click on element with name
-    Then SubjectCategories page title control
+    Then SubjectCategories page title control name
 
 
   Scenario: US_03 TC_02
     When click on element with name
-    Then  Create a Subject name as "mavi" and code as "99"
-    And Succes message should be displayed
-
-  Scenario: US_03 TC_03
-    When click on element with name
-    Then  Create a Subject name as " " and code as " "
-    And Save Button is disable
+    Then SubjectCategorie=Create a Subject name as "mavi" and code as "99"
+    And  And= Succes message should be displayed
 
 
   Scenario: US_03 TC_03
-    When click on element with name
-    Then  Create a Subject name as "mavi" and code as "99"
-    And Succes message should be displayed
+    When  click on element with name
+    Then  SubjectCategorie=Create a Subject name as " " and code as " "
+    And   Add:Save Button is disable
 
-  Scenario: Us_03 Tc_04
-    When click on element with name
-    Then  Create a Subject name2 as "mavi" and code as "99"
-    And Succes Error message should be displayed
-
+  Scenario: US_03 TC_04
+    When  click on element with name
+    Then  SubjectCategorie=Create a Subject name as "mavi" and code as "99"
+    And   !! Succes Error message should be displayed
 
   Scenario:  Us_03 Tc_05
     When click on element with name
-    When  User edit the "mavi" to "maviDünya"
-    Then Messag:Subject Category successfully updated
+    When  SubjectCategorie=User edit the "mavi" to "maviDünya"
+    Then Messag should Subject Category successfully updated
+
 
   Scenario:  Us_03 Tc_06
     When click on element with name
-    When User delete the "Tester12"
-    Then Messag:Subject Category successfully updated
+    When SubjectCategorie=User delete the "Tester12"
+    Then Messag should Subject Category successfully updated
+
 
 
   Scenario:  Us_03 Tc_07
     When click on element with name
-    When Click subject categoriy activ or inactiv
-    Then  Messag:Subject Category successfully updated
+    When SubjectCategorie=Click subject categoriy activ or inactiv
+    Then  Messag should Subject Category successfully updated
+
 
 
   Scenario:  Us_03 Tc_08
     When click on element with name
-    When When searching by  name as "kelam"
-    Then Check list with listname as "namelist" and searchtext as "kelam"
-
+    When SubjectCategorie searching by  name as "kelam"
+    Then Should=Check list with listname as "namelist" and searchtext as "kelam"
--- src/test/java/StepDefinitions/SubjectCategoriesSteps.java
+++ src/test/java/StepDefinitions/SubjectCategoriesSteps.java
@@ -21,105 +21,95 @@ public class SubjectCategoriesSteps {
     }
 
 
-//    @Then("^Create a Subject name as \"([^\"]*)\" and code as \"([^\"]*)\"$")
-//    public void createASubjectNameAsAndCodeAs(String name, String code) {
-//
-//        subjectCategoriesContent.findAndClickElement("Subject_Add");
-//        subjectCategoriesContent.findElementAndSendKeys("nameInput", name);
-//        subjectCategoriesContent.findElementAndSendKeys("codeInput", code);
-//
-//        if (subjectCategoriesContent.saveButton.isEnabled())
-//        subjectCategoriesContent.findAndClickElement("saveButton");
-//
-//    }
+    @Then("^SubjectCategorie=Create a Subject name as \"([^\"]*)\" and code as \"([^\"]*)\"$")
+    public void subjectcategorieCreateASubjectNameAsAndCodeAs(String name, String code) {
 
-    @Then("^Succes message should be displayed$")
-    public void succesMessageShouldBeDisplayed() {
-        subjectCategoriesContent.findElementAndVerifyContainsText("msjContainer", "successfully");
-
-    }
-
-    @Then("^Create a Subject name(\\d+) as \"([^\"]*)\" and code as \"([^\"]*)\"$")
-    public void createASubjectNameAsAndCodeAs(int arg0, String name, String code) {
         subjectCategoriesContent.findAndClickElement("Subject_Add");
         subjectCategoriesContent.findElementAndSendKeys("nameInput", name);
         subjectCategoriesContent.findElementAndSendKeys("codeInput", code);
 
         if (subjectCategoriesContent.saveButton.isEnabled())
+            subjectCategoriesContent.findAndClickElement("saveButton");
+
+    }
+
+    @And("^And= Succes message should be displayed$")
+    public void andSuccesMessageShouldBeDisplayed() {
+        subjectCategoriesContent.findElementAndVerifyContainsText("msjContainer", "successfully");
+
+
+
+    }
+
+
+    @When("^SubjectCategorie=User edit the \"([^\"]*)\" to \"([^\"]*)\"$")
+    public void subjectcategorieUserEditTheTo(String searcofSubjectName, String newSubjecName)  {
+
+
+        subjectCategoriesContent.editAndDeleteFunction(searcofSubjectName, "edit");
+        subjectCategoriesContent.findElementAndSendKeys("nameInput", newSubjecName);
         subjectCategoriesContent.findAndClickElement("saveButton");
 
     }
 
-    @And("^Succes Error message should be displayed$")
-    public void succesErrorMessageShouldBeDisplayed() {
-        subjectCategoriesContent.findElementAndVerifyContainsText("msjContainer", "already ");
+    @When("^SubjectCategorie=User delete the \"([^\"]*)\"$")
+    public void subjectcategorieUserDeleteThe(String nameOfSubjectToDelete)  {
 
-
-    }
-
-// Todo Edit und Delet
-
-//    @When("^User edit the \"([^\"]*)\" to \"([^\"]*)\"$")
-//    public void userEditTheTo(String searcofSubjectName, String newSubjecName) {
-//
-//        subjectCategoriesContent.editAndDeleteFunction(searcofSubjectName, "edit");
-//        subjectCategoriesContent.findElementAndSendKeys("nameInput", newSubjecName);
-//        subjectCategoriesContent.findAndClickElement("saveButton");
-//    }
-
-
-    @Then("^Messag:Subject Category successfully updated$")
-    public void messagSubjectCategorySuccessfullyUpdated() {
-
-        subjectCategoriesContent.findElementAndVerifyContainsText("msjContainer", "successfully");
-
-    }
-
-    @When("^User delete the \"([^\"]*)\"$")
-    public void userDeleteThe(String nameOfSubjectToDelete) {
 
         subjectCategoriesContent.editAndDeleteFunction(nameOfSubjectToDelete, "Delete");
         subjectCategoriesContent.findAndClickElement("yesButton");
 
     }
 
-    @When("^Click subject categoriy activ or inactiv$")
-    public void clickSubjectCategoriyActivOrInactiv() {
+    @And("^Add:Save Button is disable$")
+    public void addSaveButtonIsDisable() {
+        subjectCategoriesContent.elementisEnabled("saveButton");
+
+
+
+    }
+
+    @Then("^SubjectCategories page title control name$")
+    public void subjectcategoriesPageTitleControlName() {
+
+        Assert.assertTrue(subjectCategoriesContent.pageTitle.getText().contains("Subject Categories"));
+
+    }
+
+    @Then("^Messag should Subject Category successfully updated$")
+    public void messagShouldSubjectCategorySuccessfullyUpdated() {
+
+        subjectCategoriesContent.findElementAndVerifyContainsText("msjContainer", "successfully");
+
+
+    }
+
+    @When("^SubjectCategorie searching by  name as \"([^\"]*)\"$")
+    public void subjectcategorieSearchingByNameAs(String name) {
+
+
+        subjectCategoriesContent.findElementAndSendKeys("searchName",name);
+        subjectCategoriesContent.findAndClickElement("searchButton");
+
+
+    }
+
+    @Then("^Should=Check list with listname as \"([^\"]*)\" and searchtext as \"([^\"]*)\"$")
+    public void shouldCheckListWithListnameAsAndSearchtextAs(String listname, String text) {
+        subjectCategoriesContent.checklist(listname,text);
+    }
+
+    @And("^!! Succes Error message should be displayed$")
+    public void succesErrorMessageShouldBeDisplayed() {
+        subjectCategoriesContent.findElementAndVerifyContainsText("msjContainer", "already ");
+
+    }
+
+    @When("^SubjectCategorie=Click subject categoriy activ or inactiv$")
+    public void subjectcategorieClickSubjectCategoriyActivOrInactiv() {
 
         subjectCategoriesContent.findAndClickElement("activeButton");
 
-
-
-    }
-
-//    @When("^When searching by  name as \"([^\"]*)\"$")
-//    public void whenSearchingByNameAs(String name) {
-//
-//        subjectCategoriesContent.findElementAndSendKeys("searchName",name);
-//        subjectCategoriesContent.findAndClickElement("searchButton");
-//
-//
-//    }
-
-    @And("^Save Button is disable$")
-    public void saveButtonIsDisable() {
-
-
-subjectCategoriesContent.elementisEnabled("saveButton");
-
-    }
-
-//    @Then("^Check list with listname as \"([^\"]*)\" and searchtext as \"([^\"]*)\"$")
-//    public void checkListWithListnameAsAndSearchtextAs(String listname, String text) {
-//
-//        subjectCategoriesContent.checklist(listname,text);
-//
-//    }
-
-    @Then("^SubjectCategories page title control$")
-    public void subjectcategoriesPageTitleControl() {
-
-
-       Assert.assertTrue(subjectCategoriesContent.pageTitle.getText().contains("Subject Categories"));
     }
 }
+
